aba1f4bcf6ee11162ee6b438df3fc7ebe0602b39
PAAWSA02LC0912 <Duoc@PAAWSA02LC0912.duoc.acad>
2025-10-06 21:02:35 -0300

    mi segundo commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='row  mt-3'> {/* ROW ES EL RECUADRO DEL TITULO, POR LO QUE EL 
+        MT3 VA EN EL ROW YA QUE FUNCIONA DE ARRIBA HACIA ABAJO */}
+          <div className='col-lg-12'>
+            <h2>Pagina de calculo de remuneraciones</h2>
+          </div>
+        </div>
+      </div>
+
+      
+      {/* CREACION DE OTRA COLUMNA CON DOS FILAS SEPARADAS */}
+        <div className='row  mt-3'>{/* FILA NUEVA*/}
+          <div className='col-lg-6'>{/* PRIMERA COLUMNA*/}
+            <h2>Datos Sueldo Bruto</h2>
+            <br></br>
+            <label htmlFor='Sueldo Bruto'>Ingrese sueldo bruto</label>
+            <input type='number' id='sueldo' placeholder='Ingrese sueldo bruto'></input>
+          </div>
+          <div className='col-lg-6'>{/* SEGUNDA COLUMNA */}
+            <h2>Resultados</h2>
+          </div>
+        </div>
+      
     </div>
   );
 }
